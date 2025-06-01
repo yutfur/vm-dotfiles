cdf88d7eebf6e2ea82eeeb57c947cb805d9b4be2
yutfur <hello@yutfur.com>
2025-06-02 08:50:33 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -6,61 +6,61 @@ sudo pacman -Syu
 # Package list
 packagelist=(
 
-   # Hardware
-   "mesa"
-   "xf86-video-vmware"
-   "open-vm-tools"
-   "gtkmm3"
-   "xf86-input-vmmouse"
-   "imwheel"
-   #"ntfs-3g"
+    # Hardware
+    "mesa"
+    "xf86-video-vmware"
+    "open-vm-tools"
+    "gtkmm3"
+    "xf86-input-vmmouse"
+    "imwheel"
+    #"ntfs-3g"
 
-   # Wayland
-   #"xorg-xwayland"
+    # Wayland
+    #"xorg-xwayland"
 
-   # UI
+    # UI
 
-   # Audio
-   "pipewire"
+    # Audio
+    "pipewire"
 
-   # Image
+    # Image
 
-   # Security
-   #"clamav"
-   #"ufw"
+    # Security
+    #"clamav"
+    #"ufw"
 
-   # IME
-   # Enter a selection に 1-4 (全て) と入力する
-   "fcitx5-im"
-   "fcitx5-mozc"
+    # IME
+    # Enter a selection に 1-4 (全て) と入力する
+    "fcitx5-im"
+    "fcitx5-mozc"
 
-   # Misc, Tools
-   "gvim"
-   "man-db"
-   "man-pages"
-   "pacman-contrib"
-   "hugo"
-   "copyq"
-   "chromium"
+    # Misc, Tools
+    "gvim"
+    "man-db"
+    "man-pages"
+    "pacman-contrib"
+    "hugo"
+    "copyq"
+    "chromium"
 
-   # Terminal, Shell, Command
-   "zellij"
-   "fzf"
+    # Terminal, Shell, Command
+    "zellij"
+    "fzf"
 
-   # Dev
-   "git"
+    # Dev
+    "git"
 
-   # Java
-   "jdk17-openjdk"
+    # Java
+    "jdk17-openjdk"
 
-   # DB
+    # DB
 
 )
 
 # Install packages
 for packages in "${packagelist[@]}"
 do
-   sudo pacman -S --noconfirm "${packages}"
+    sudo pacman -S --noconfirm "${packages}"
 done
 
 # Update & Upgrade packages (AUR)
@@ -70,14 +70,14 @@ yay -Syu
 # AUR package list
 aurs=(
 
-   "visual-studio-code-bin"
+    "visual-studio-code-bin"
 
 )
 
 # Install AUR packages
 for packages in "${aurs[@]}"
 do
-   yay -S "${packages}"
+    yay -S "${packages}"
 done
 
 # Clean unneeded dependencies
